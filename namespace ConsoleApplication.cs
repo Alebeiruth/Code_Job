439780573306af650a330352f26b7b938c8203b5
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2023-10-11 16:33:42 +0000

    primeiro commit

---
+++ namespace ConsoleApplication.cs
@@ -0,0 +1,52 @@
+namespace ConsoleApplication;
+
+class Program {
+    static void Main(string[] args) {
+        string pastaProcura = @"\\profiles\usersprofiles$\tc829242\Desktop\pasta_Teste";
+
+        if (Directory.Exists(pastaProcura)) {
+
+            DateTime dataCriacao = DateTime.Now.AddDays(-1);
+
+            string[] arquivos = Directory.GetFiles(pastaProcura, "*.txt", SearchOption.AllDirectories);
+
+            foreach (string arquivo in arquivos) {
+
+                FileInfo info = new FileInfo(arquivo);
+
+                if (info.CreationTime < dataCriacao) {
+
+                    Console.WriteLine("Arquivo: {0} - Data: {1}", info.Name, info.CreationTime);
+
+                    File.Delete(arquivo);
+                }
+            }
+            Console.WriteLine("Arquivos antigos foram deletados. ");
+        }
+        else {
+            Console.WriteLine("Pasta não existe. ");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
